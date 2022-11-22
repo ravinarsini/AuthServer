dffbdce575f9119fcd5846cfce8a0fe1e12e258b
Ravi Narsini <ravi.narsi@ibridgetechsoft.com>
2022-11-22 11:29:35 +0530

    Revert "intial checkin"
    This reverts commit fe1a7fb861624ebe8f4b1ba9a8333219168a970a.

--- BasicAuthSample/DBScripts/dbo.UserRole.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE dbo.UserRole
-(
-	UserId INT NOT NULL,
-	RoleId INT NOT NULL,
-
-	CONSTRAINT PK_UserRole PRIMARY KEY(UserId, RoleId),
-	CONSTRAINT FK_UserRole_Login FOREIGN KEY(UserId) REFERENCES dbo.Login(Id),
-	CONSTRAINT FK_UserRole_Role FOREIGN KEY(RoleId) REFERENCES dbo.Role(Id)
-)
